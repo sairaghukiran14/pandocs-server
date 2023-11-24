1d8df6004c0e7e32880d4665b196349c106d04bf
Avula Sai Raghu Kiran <106566627+sairaghukiran14@users.noreply.github.com>
2023-11-24 23:18:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,13 @@ import cors from "cors";
 import Document from "./models/Document.js";
 const defaultValue = "";
 const app = express();
-app.use(cors());
+const corsOptions = {
+  origin: "https://pandocs-client.vercel.app/",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+// app.use(cors());
 connectDB();
 // import Document from './models/Document.js';
 // import documentRouter from './routes/documentRouter.js';
